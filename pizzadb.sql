a97d2961d63b338c542f3aa2a7e6b385dcf8bdbb
Anu1495 <141367947+Anu1495@users.noreply.github.com>
2023-09-25 02:45:53 +0530

    Add files via upload

--- pizzadb.sql
+++ pizzadb.sql
@@ -1,31 +1,83 @@
-select Round(sum(total_price),2) Total_Revenue from pizza_sales
-select * from pizza_sales
-select sum(total_price)/count(distinct order_id) from pizza_sales
-select count(distinct order_id) from pizza_sales
-select cast(cast(sum(quantity) as decimal (10,2))/count( distinct order_id) as decimal (10,2)) from pizza_sales
-select datename(month, order_date) as Order_month, count (distinct order_id) total_orders
+--Total Revenue:
+SELECT SUM(total_price) AS Total_Revenue FROM pizza_sales;
+
+--Average Order Value
+SELECT (SUM(total_price) / COUNT(DISTINCT order_id)) AS Avg_order_Value FROM pizza_sales
+
+--Total Pizzas Sold
+SELECT SUM(quantity) AS Total_pizza_sold FROM pizza_sales
+
+--Total Orders
+SELECT COUNT(DISTINCT order_id) AS Total_Orders FROM pizza_sales
+
+--Average Pizzas Per Order
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
+AS Avg_Pizzas_per_order
+FROM pizza_sales
+
+--Daily Trend for Total Orders
+SELECT DATENAME(DW, order_date) AS order_day, COUNT(DISTINCT order_id) AS total_orders 
+FROM pizza_sales
+GROUP BY DATENAME(DW, order_date)
+
+--Monthly Trend for Orders
+select DATENAME(MONTH, order_date) as Month_Name, COUNT(DISTINCT order_id) as Total_Orders
 from pizza_sales
-group by datename(month, order_date)
-order by total_orders desc
-percentage of sales by pizza category
-select * from [pizza db].[dbo].[pizza_sales]
-select pizza_category, sum(total_price),round(sum(total_price)*100/(select sum(total_price) from pizza_sales where month(order_date)=3),2)
-from pizza_sales
-where month(order_date)=3
-group by pizza_category
-select pizza_size, sum(total_price), sum(total_price)*100/(select sum(total_price) from pizza_sales)
-from pizza_sales
-where datepart(quarter,order_date)=3
-group by pizza_size
-select top 5 pizza_name, sum(quantity) as Quantity
-from pizza_sales
-group by pizza_name
-order by quantity desc
-select top 5 pizza_name, sum(total_price) as price
-from pizza_sales
-group by pizza_name
-order by price desc
-select top 5 pizza_name, count(distinct order_id) as Orders
-from pizza_sales
-group by pizza_name
-order by orders desc
+GROUP BY DATENAME(MONTH, order_date)
+
+--% of Sales by Pizza Category
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_category
+
+-- % of Sales by Pizza Size
+SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_size
+ORDER BY pizza_size
+
+--Total Pizzas Sold by Pizza Category
+SELECT pizza_category, SUM(quantity) as Total_Quantity_Sold
+FROM pizza_sales
+WHERE MONTH(order_date) = 2
+GROUP BY pizza_category
+ORDER BY Total_Quantity_Sold DESC
+
+--Top 5 Pizzas by Revenue
+SELECT Top 5 pizza_name, SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue DESC
+
+--Bottom 5 Pizzas by Revenue
+SELECT Top 5 pizza_name, SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue
+
+--Top 5 Pizzas by Quantity
+SELECT Top 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold DESC
+
+--Bottom 5 Pizzas by Quantity
+SELECT TOP 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold ASC
+
+--Top 5 Pizzas by Total Orders
+SELECT Top 5 pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders DESC
+
+--Borrom 5 Pizzas by Total Orders
+SELECT Top 5 pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders ASC
